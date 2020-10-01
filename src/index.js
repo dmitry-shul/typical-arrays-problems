4fb5391536c07a9462f84aaef68cae428840bf71
dmitry-sh <matemV@mail.ru>
2020-10-01 19:01:04 +0300

    feat: Add program in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+    let i = 0;
+    let minNumber;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    minNumber = array[0]; 
+    while (i < array.length) {
+        if (array[i] <= minNumber) {
+            minNumber = array[i];
+        } 
+        i = i + 1;
+    }
+        return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let i = 0;
+    let maxNumber;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    maxNumber = array[0];
+    while (i < array.length) {
+        if (array[i] >= maxNumber) {
+            maxNumber = array[i];
+        }
+        i++;
+    }
+  return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let i = 0;
+    let avgNumber = 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    while (i < array.length) {
+        avgNumber = avgNumber + array[i];
+        i++;
+    }
+    avgNumber = avgNumber / array.length;
+  return avgNumber;
 }
